ae5e40534b39b1091252c337996485b11df777ee
Loc <loc.tntran@gmail.com>
2026-01-12 17:26:47 -0600

    First commit

---
+++ src/main/java/loctran/showhub/ShowHubApplication.java
@@ -0,0 +1,13 @@
+package loctran.showhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/loctran/showhub/ShowHubApplicationTests.java
@@ -0,0 +1,13 @@
+package loctran.showhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
