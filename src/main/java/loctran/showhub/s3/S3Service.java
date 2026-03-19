a3173f4cef5bb3cd6497b015d076be177b221d43
Loc <loc.tntran@gmail.com>
2026-03-19 18:01:17 -0500

    add new methods to s3Services

--- src/main/java/loctran/showhub/s3/S3Service.java
+++ src/main/java/loctran/showhub/s3/S3Service.java
@@ -1,19 +1,35 @@
 package loctran.showhub.s3;
 
+import io.jsonwebtoken.lang.Collections;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 import java.io.IOException;
-import java.util.UUID;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class S3Service {
     private S3Client s3Client;
+
+    @Value("${aws.region}")
+    private String region;
 
     public void uploadFile(String bucketName, String key, MultipartFile file) throws IOException {
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
@@ -22,5 +38,56 @@ public class S3Service {
                 .build();
 
         s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+    }
+
+    public String generatePresigned(String bucketName, String key, Duration duration){
+        try(S3Presigner presigner = S3Presigner.builder()
+                .region(Region.of(region))
+                .build()) {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            GetObjectPresignRequest presignedRequest = GetObjectPresignRequest.builder()
+                    .signatureDuration(duration)
+                    .getObjectRequest(getObjectRequest)
+                    .build();
+
+            PresignedGetObjectRequest presigned = presigner.presignGetObject(presignedRequest);
+
+            return presigned.url().toString();
+        }
+    }
+
+    public void deleteFile(String bucketName, String key){
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+
+    public byte[] downloadFile(String bucketName, String key){
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        ResponseBytes<GetObjectResponse> responseBytes = s3Client.getObjectAsBytes(getObjectRequest);
+
+        return responseBytes.asByteArray();
+    }
+
+    public List<String> listFiles(String bucketName, String prefix){
+        ListObjectsV2Request request =  ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .prefix(prefix)
+                .build();
+
+        ListObjectsV2Response response = s3Client.listObjectsV2(request);
+
+        return response.contents().stream().map(S3Object::key).collect(Collectors.toList());
     }
 }
